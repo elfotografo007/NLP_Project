a244f524eb543148e45da478c49358cb1a9ebcac
Andres Torres <elfotografo007@gmail.com>
2017-10-06 17:32:43 +0200

    Now we tokenize and create bigrams

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
-
+import nltk
+from nltk import word_tokenize
+from nltk.util import ngrams
+import csv
 # steps:
 # 1. tokenize
 # 2. N-grams (2-grams and 3-grams) 
@@ -9,4 +12,15 @@
 # 6. Maybe try word2vec if there is time
 
 if __name__ == '__main__':
-    print('Hello Somto')
+    with open('uci-news-aggregator.csv', 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        inputs = []
+        targets = []
+        for row in reader:
+            inputs.append(row[1])
+            targets.append(row[4])
+        
+    bigrams = [ngrams(word_tokenize(s),2, pad_left=True, pad_right=True, left_pad_symbol='<s>', right_pad_symbol='</s>') for s in inputs]
+    print(list(bigrams[1]))
+    
+    
