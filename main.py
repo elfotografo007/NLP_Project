1e45fec46db1496627a6d383e26103b96f49ddea
Andres Torres <elfotografo007@gmail.com>
2017-10-06 16:47:01 +0200

    Added son basic steps for the main

---
+++ main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+# steps:
+# 1. tokenize
+# 2. N-grams (2-grams and 3-grams) 
+# 3. select params using 5-fold
+# 4. Train
+# 5. classify
+# 6. Maybe try word2vec if there is time
+
+if __name__ == '__main__':
+    print('Hello Somto')
