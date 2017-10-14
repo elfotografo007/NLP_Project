caf1b66ed54b270a85ea107d30e6e3d50dd1c913
Andres Torres <elfotografo007@gmail.com>
2017-10-14 18:16:45 +0200

    Now the base model class stores and loads

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.externals import joblib
 '''
 Created on Oct 6, 2017
 
@@ -24,8 +25,11 @@ class Model(object):
     def printConfusion(self, inputs, targets):
         print(confusion_matrix(targets, self.classify(inputs)))
     
-    def save(self):
-        pass
+    def save(self, filename):
+        joblib.dump(self, filename)
     
-    def load(self):
-        pass
+    @staticmethod
+    def load(filename):
+        return joblib.load(filename)
+        
+        
