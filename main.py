6d92d6deb35f054f9c33c42290f2fe20c56dfc02
Andres Torres <elfotografo007@gmail.com>
2017-10-14 17:47:40 +0200

    Basic main.py modified

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-import nltk
-from nltk import word_tokenize
-from nltk.util import ngrams
+import numpy as np
 import csv
+from logistic_model import LogisticRegressionModel
+from sklearn.model_selection import train_test_split
 # steps:
 # 1. tokenize
 # 2. N-grams (2-grams and 3-grams) 
@@ -19,8 +19,22 @@ if __name__ == '__main__':
         for row in reader:
             inputs.append(row[1])
             targets.append(row[4])
+    # Split dataset into Training and test
+    training, test, l_training, l_test = train_test_split(inputs, targets, test_size=0.2)
         
-    bigrams = [ngrams(word_tokenize(s),2, pad_left=True, pad_right=True, left_pad_symbol='<s>', right_pad_symbol='</s>') for s in inputs]
-    print(list(bigrams[1]))
-    
-    
+    lm = LogisticRegressionModel(inputs)
+     
+    lm.train(training, l_training)
+    y = lm.classify(test)
+    good =0
+    for yn, t in zip(y,l_test):
+        if yn == t:
+            good += 1
+    print(good/len(y))
+    print(lm.getMetrics(test, l_test))
+    print(lm.classify(['Today’s Top Supply Chain and Logistics News From WSJ']))
+    print(lm.classify(['Facebook Users Were Unwitting Targets of Russia-Backed Scheme']))
+    print(lm.classify(['Explaining Health Insurance Cost-Sharing Reductions']))
+    print(lm.classify(["Nelly's Rape Accuser Says She Will NOT Testify, Wants to Drop the Case"]))
+          
+          
