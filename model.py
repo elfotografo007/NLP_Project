8f1da91fe581539f46c99876ffa7186f5bc5878a
Andres Torres <elfotografo007@gmail.com>
2017-10-18 16:15:15 +0200

    Confusion matrix printing improved

--- model.py
+++ model.py
@@ -22,8 +22,13 @@ class Model(object):
     def getMetrics(self, inputs, targets):
         return classification_report(targets, self.classify(inputs))
     
-    def printConfusion(self, inputs, targets):
-        print(confusion_matrix(targets, self.classify(inputs)))
+    def printConfusion(self, inputs, targets, labels=None):
+        if labels is None:
+            conf = confusion_matrix(targets, self.classify(inputs))
+        else:
+            conf = confusion_matrix(targets, self.classify(inputs), labels=labels)
+            print(labels)
+        print(conf)
     
     def save(self, filename):
         joblib.dump(self, filename)
